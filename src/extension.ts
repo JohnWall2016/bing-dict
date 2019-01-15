5805928331c4cc5f16260a0f38b2b677ff4a855f
John Wall <5672618@qq.com>
2019-01-15 23:43:20 +0800

    update

--- src/extension.ts
+++ src/extension.ts
@@ -9,14 +9,15 @@ export function activate(context: vscode.ExtensionContext) {
     
     context.subscriptions.push(vscode.commands.registerCommand(commandId, async () => {
         let value = '';
-
         let editor: vscode.TextEditor;
         if (editor = vscode.window.activeTextEditor) {
             const selection = editor.selection;
             if (selection.start.line === selection.end.line) {
                 if (selection.isEmpty) {
                     let range = editor.document.getWordRangeAtPosition(selection.start);
-                    value = editor.document.getText(range);
+                    if (range) {
+                        value = editor.document.getText(range);
+                    }
                 } else {
                     value = editor.document.getText(selection);
                 }
@@ -27,36 +28,89 @@ export function activate(context: vscode.ExtensionContext) {
             value, placeHolder: 'Input a word to lookup definition'
         });
 
-        const definition = await getDefinition(word);
-
-        vscode.window.showInformationMessage(definition);
+        if (word) {
+            const definition = await getDefinition(word);
+            vscode.window.showInformationMessage(definition);
+        }
         
     }));
 }
 
 function getDefinition(word) {
-    return new Promise((resolve: (arg: string) => void, reject) => {
-        let result = '', definition = '';
+    return new Promise((resolve: (arg: string) => void) => {
+        let result = '', definition = '', pronunciation = '';
         http.get(`http://www.bing.com/dict/search?mkt=zh-cn&q=${escape(word)}`, res => {
             res.on('data', (chunk) => { result += chunk; });
             res.on('end', () => {
+                console.log(result);
                 let regex = new RegExp(
-                    `span class="pos.*?">(.*?)</span>.*?<span class="def"><span>(.*?)</span></span></li>`, 
+                    `span class="pos.*?">(.*?)</span>.*?<span class="def">(.*?)</span></li>`, 
                     'g'
                 );
-                let m;
+                let m: RegExpExecArray;
+                // definitions
                 while (m = regex.exec(result)) {
                     if (definition) definition += ' | ';
-                    definition += `${m[1]} ${m[2]}`;
+                    definition += `${trimHtmlTag(m[1])} ${trimHtmlTag(m[2])}`;
                 }
+                // pronunciation
                 if (m = /<div class="hd_prUS[^\[]*(\[.*?\])/.exec(result)) {
-                    definition = m[1] + ' ' + definition;
-                } else if (m = (/hd_p1_1" lang="en">(.*?)<\/div/).exec(result)) {
-                    definition = m[1] + ' ' + definition;
+                    pronunciation = _unescape(m[1]);
+                } else if (m = /hd_p1_1" lang="en">([^<>]*?)<\/div/.exec(result)) {
+                    pronunciation = _unescape(m[1]);
                 }
-                resolve(definition);
+                resolve(`${word} ${pronunciation} : ${definition}`);
             });
         });
     });
 }
 
+function trimHtmlTag(str: string) {
+    return str.replace(/<[^>]*>/g, '');
+}
+
+const unescapeMap = {
+    ['&amp;']: '&',
+    ['&hellip;']: '...',
+    ['&quot;']: '"',
+    ['&#([0-9]*);']: (match: RegExpExecArray) => {
+        return String.fromCharCode(Number(match[1]));
+    }
+}
+
+class UnescapeMap {
+    map: {}
+    regexp: RegExp;
+
+    constructor(map = unescapeMap) {
+        this.map = map;
+        this.regexp = new RegExp(Object.keys(map).join('|'), 'g');
+    }
+
+    replace(str: string) {
+        let ret = '';
+        let index = 0;
+        let m: RegExpExecArray;
+        while (m = this.regexp.exec(str)) {
+            ret += str.substring(index, m.index);
+            for (let key of Object.keys(this.map)) {
+                if (new RegExp(`^${key}$`).test(m[0])) {
+                    if (typeof this.map[key] == 'function') {
+                        ret += this.map[key](m);
+                    } else {
+                        ret += this.map[key];
+                    }
+                    break;
+                }
+            }
+            index = this.regexp.lastIndex;
+        }
+        ret += str.substring(index);
+        return ret;
+    }
+}
+
+function _unescape(str: string) {
+    const map = new UnescapeMap();
+    return map.replace(str);
+}
